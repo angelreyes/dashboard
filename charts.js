b93ce3e0fa44ff31c5fa537e0605c8e5973cd58e
root <root@cajadearena.angelreyes.me>
2014-05-03 05:02:59 +0600

    doStats renamed as doKpi, passing data struct as param

--- charts.js
+++ charts.js
@@ -1,37 +1,37 @@
-function doStats(div) {
-
+function doKpi(div, opts) {
 	var staticsOverview = {
-
 		init: function() {
-			console.log("init");
-			this.drawChart(div);
+			console.log("init drawing of chart: " + div + " opts: " + opts);
+			this.drawChart(div, opts);
 		},
-
-		myAlert: function () {
-			alert( "inside my alert" );
-			console.log("inside my alert" );
+		myAlert: function (msg) {
+			alert( "inside my alert: " + msg );
+			console.log("inside my alert: " + msg);
 		},
-
-		drawChart: function (chartDiv) {
-
+		drawChart: function (chartDiv, opts) {
 			var jsonData = $.ajax({
 				url: "data.json",
 				dataType:"json",
 				async: false
 			}).responseText;
-
 			var options = {
-			title: 'Your Chart Title'
+				title: 'Your Chart Title'
 			};
-
 			var data = new google.visualization.DataTable(jsonData);
-
-			var chart = new google.visualization.LineChart(chartDiv);
-
+			if(!("type" in opts)) { opts["type"] == undefined; }  
+			switch(opts["type"])
+			{
+				case "column":
+					var chart = new google.visualization.ColumnChart(chartDiv);
+				break;
+				case "line":
+					var chart = new google.visualization.LineChart(chartDiv);
+				break;
+				default:
+					var chart = new google.visualization.LineChart(chartDiv);
+			}
 			chart.draw(data, options);
 		},
-
 	};
-
 	staticsOverview.init();
 }
